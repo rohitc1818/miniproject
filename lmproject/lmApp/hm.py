4f4a07165392ccd3b2dcfa4c82e998745b90724e
choudharirohit1995 <rc5741545@gmail.com>
2021-03-08 17:27:57 +0530

    oil

--- lmproject/lmApp/hm.py
+++ lmproject/lmApp/hm.py
@@ -2,4 +2,7 @@ l1 = int(input('Enter a no:'))
 if l1 == 4:
     print('Four')
 else:
+    thirsty = input('are you thirsty?')
+    if thirsty == 'yes':
+        print('Drink oil')
     print('This is the',l1)
