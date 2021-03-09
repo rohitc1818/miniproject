24e877f584a9498440a769854ab7f242ea314405
choudharirohit1995 <rc5741545@gmail.com>
2021-03-09 12:45:31 +0530

    dd

--- lmproject/lmApp/hm.py
+++ lmproject/lmApp/hm.py
@@ -4,14 +4,11 @@ if l1 == 4:
     print('sigma')
     print('dont do this')
 else:
-<<<<<<< HEAD
     if l1 == 1:
         print('This is 1')
     print('This is the',l1)
-=======
     dummy = input('Enter a name:')
     if dummy == 'Rohan':
         print('This is the dummy name')
     else:
         print('This is the',l1)
->>>>>>> dummy
